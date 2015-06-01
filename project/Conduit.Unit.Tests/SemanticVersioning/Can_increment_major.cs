65628a1930ae993a16f9691984a2f4d6e3592871
Ben Biddington <ben.biddington@xero.com>
2015-06-02 10:26:36 +1200

    [DRAFT] SemanticVersioning, Can_increment_major

---
+++ project/Conduit.Unit.Tests/SemanticVersioning/Can_increment_major.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Semver;
+
+namespace Conduit.Unit.Tests.SemanticVersioning
+{
+    [TestFixture]
+    public class Can_increment_major
+    {
+        [Test]
+        public void like_this()
+        {
+            var versionOne = new SemVersion(1);
+
+            var versionTwo = BumpMajor(versionOne);
+
+            Assert.That(versionTwo.Major, Is.EqualTo(2));
+        }
+
+        private SemVersion BumpMajor(SemVersion versionOne)
+        {
+            return new SemVersion(versionOne.Major + 1);
+        }
+
+        // TEST: it leaves the value unchanged
+    }
+}
