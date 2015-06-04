bd0a0e9dd4f27325f15b8bb0414dc7fac551e0c2
Ben Biddington <ben.biddington@xero.com>
2015-06-04 16:32:38 +1200

    Refactor: duplication

--- Conduit.Integration.Tests/Versioning/AssemblyFileVersion.cs
+++ Conduit.Integration.Tests/Versioning/AssemblyFileVersion.cs
@@ -1,41 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
+﻿using Conduit.Integration.Tests.Versioning.Private;
 using Conduit.UseCases.Semver.Semver;
 
 namespace Conduit.Integration.Tests.Versioning
 {
 	internal static class AssemblyFileVersion
 	{
-		private const string VERSION_PATTERN = @"\(""(?<versionstring>([^.]+).([^.]+).([^.]+)).([^.]+)""\)";
-
 		public static SemVersion For(string filename)
 		{
-			var lines = File.ReadAllLines(filename);
-
-			var assemblyFileVersionPattern = new Regex(string.Format("AssemblyFileVersion{0}", VERSION_PATTERN), RegexOptions.Compiled);
-
-			foreach (var line in lines.Where(it => false == string.IsNullOrEmpty(it)))
-			{
-				Match match = assemblyFileVersionPattern.Match(line);
-
-				if (match.Success)
-				{
-					var version = match.Groups["versionstring"].Value;
-
-					try
-					{
-						return SemVersion.Parse(version);
-					}
-					catch (Exception)
-					{
-						throw new Exception("Failed to parse ethis text to version <" + version + ">");
-					}
-				}
-			}
-
-			return new SemVersion(0);
+			return AssemblyInfoVersion.For(filename, "AssemblyFileVersion");
 		}
 	}
 }
---
+++ Conduit.Integration.Tests/Versioning/Private/AssemblyInfoVersion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Conduit.UseCases.Semver.Semver;
+
+namespace Conduit.Integration.Tests.Versioning.Private
+{
+	internal static class AssemblyInfoVersion
+	{
+		private const string VERSION_PATTERN = @"\(""(?<versionstring>([^.]+).([^.]+).([^.]+)).([^.]+)""\)";
+
+		public static SemVersion For(string filename, string prefix)
+		{
+			var lines = File.ReadAllLines(filename);
+
+			var assemblyFileVersionPattern = new Regex(string.Format("{0}{1}",prefix, VERSION_PATTERN), RegexOptions.Compiled);
+
+			foreach (var line in lines.Where(it => false == string.IsNullOrEmpty(it)))
+			{
+				var match = assemblyFileVersionPattern.Match(line);
+
+				if (match.Success)
+				{
+					return SemVersionFrom(match);
+				}
+			}
+
+			return new SemVersion(0);
+		}
+
+		private static SemVersion SemVersionFrom(Match match)
+		{
+			var version = match.Groups["versionstring"].Value;
+
+			try
+			{
+				return SemVersion.Parse(version);
+			}
+			catch (Exception)
+			{
+				throw new Exception("Failed to parse ethis text to version <" + version + ">");
+			}
+		}
+	}
+}
