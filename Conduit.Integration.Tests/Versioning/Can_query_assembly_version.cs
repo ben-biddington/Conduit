1cec714870fa5e4297eb6edacb68902c249723cc
Ben Biddington <ben.biddington@xero.com>
2015-06-04 16:07:40 +1200

    Versioning, version_and_file_version_are_both_available_separately

--- Conduit.Integration.Tests/Versioning/Can_query_assembly_version.cs
+++ Conduit.Integration.Tests/Versioning/Can_query_assembly_version.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 using Semver;
@@ -37,6 +36,20 @@ namespace Conduit.Integration.Tests.Versioning
 			Assert.That(version, Is.EqualTo(SemVersion.Parse("1.337.0", true)));
 		}
 
+		[Test]
+		public void version_and_file_version_are_both_available_separately()
+		{
+			FileMachine.Make("AssemblyInfo.cs", @"
+				[assembly: AssemblyVersion(""1.337.0.0"")]
+				[assembly: AssemblyFileVersion(""1.338.0.0"")]");
+
+			var version = AssemblyVersion.For("AssemblyInfo.cs");
+			var fileVersion = AssemblyFileVersion.For("AssemblyInfo.cs");
+
+			Assert.That(version, Is.EqualTo(SemVersion.Parse("1.337.0", true)));
+			Assert.That(fileVersion, Is.EqualTo(SemVersion.Parse("1.338.0", true)));
+		}
+
 		// TEST: it fails when the file does not exist
 		// TEST: it ignores anything after patch because <SemVersion> does not like it
 	}
@@ -49,11 +62,11 @@ namespace Conduit.Integration.Tests.Versioning
 		{
 			var lines = File.ReadAllLines(filename);
 
-			var assembltFileVersionPattern = new Regex(string.Format("AssemblyFileVersion{0}", VERSION_PATTERN), RegexOptions.Compiled);
+			var assemblyVersionPattern = new Regex(string.Format("AssemblyVersion{0}", VERSION_PATTERN), RegexOptions.Compiled);
 
 			foreach (var line in lines.Where(it => false == string.IsNullOrEmpty(it)))
 			{
-				Match match = assembltFileVersionPattern.Match(line);
+				Match match = assemblyVersionPattern.Match(line);
 
 				if (match.Success)
 				{
@@ -65,7 +78,39 @@ namespace Conduit.Integration.Tests.Versioning
 					}
 					catch (Exception)
 					{
-						
+						throw new Exception("Failed to parse ethis text to version <" + version + ">");
+					}
+				}
+			}
+
+			return new SemVersion(0);
+		}
+	}
+
+	internal static class AssemblyFileVersion
+	{
+		private const string VERSION_PATTERN = @"\(""(?<versionstring>([^.]+).([^.]+).([^.]+)).([^.]+)""\)";
+
+		public static SemVersion For(string filename)
+		{
+			var lines = File.ReadAllLines(filename);
+
+			var assemblyFileVersionPattern = new Regex(string.Format("AssemblyFileVersion{0}", VERSION_PATTERN), RegexOptions.Compiled);
+
+			foreach (var line in lines.Where(it => false == string.IsNullOrEmpty(it)))
+			{
+				Match match = assemblyFileVersionPattern.Match(line);
+
+				if (match.Success)
+				{
+					var version = match.Groups["versionstring"].Value;
+
+					try
+					{
+						return SemVersion.Parse(version);
+					}
+					catch (Exception)
+					{
 						throw new Exception("Failed to parse ethis text to version <" + version + ">");
 					}
 				}
