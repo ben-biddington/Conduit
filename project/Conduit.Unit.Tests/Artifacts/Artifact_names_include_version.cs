6f046dcfdc390c2189cf43f16cf88850b14c7863
Ben Biddington <ben.biddington@gmail.com>
2015-06-06 11:37:12 +1200

    [DRAFT] Artifact_names_include_source_control_branch_name

--- project/Conduit.Unit.Tests/Artifacts/Artifact_names_include_version.cs
+++ project/Conduit.Unit.Tests/Artifacts/Artifact_names_include_version.cs
@@ -11,24 +11,50 @@ namespace Conduit.Unit.Tests
 		{
 			var version = new SemVersion(1);
 
-			var name = new ArtifactName(version);
+			var name = new ArtifactName(version, SourceControlBranch.Master);
 
 			Assert.That (name.ToString(), Is.StringContaining("1.0.0"));
+		}
+	}
+
+	public class Artifact_names_include_source_control_branch_name
+	{
+		[Test]
+		public void for_example() 
+		{
+			var branchName = new SourceControlBranch ("next");
+
+			var name = new ArtifactName (new SemVersion (1), branchName);
+
+			Assert.That(name.ToString(), Is.StringContaining("next"));
+		}
+	}
+
+	class SourceControlBranch
+	{
+		public static readonly SourceControlBranch Master = new SourceControlBranch ("master"); 
+		public string Name { get; private set; }
+
+		internal SourceControlBranch(string name)
+		{
+			Name = name;
 		}
 	}
 
 	class ArtifactName 
 	{
 		private readonly SemVersion _version;
+		private readonly SourceControlBranch _branch;
 
-		internal ArtifactName(SemVersion version)
+		internal ArtifactName(SemVersion version, SourceControlBranch branch)
 		{
 			_version = version;
+			_branch = branch;
 		}
 
 		public override string ToString()
 		{
-			return _version.ToString();
+			return string.Join(string.Empty,_version.ToString(), _branch.Name);
 		}
 	}
 }
