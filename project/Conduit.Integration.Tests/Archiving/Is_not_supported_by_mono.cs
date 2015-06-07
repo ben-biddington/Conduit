8c5f259e48545855908e6b14e1ca3a5e49a4716d
Ben Biddington <ben.biddington@gmail.com>
2015-06-08 11:48:33 +1200

    Failure diagnostics

--- project/Conduit.Integration.Tests/Archiving/Is_not_supported_by_mono.cs
+++ project/Conduit.Integration.Tests/Archiving/Is_not_supported_by_mono.cs
@@ -29,11 +29,14 @@ namespace Conduit.Integration.Tests.Archiving
 		{
 			FileMachine.Make("README.md", @"Expect this to be added to the archive");
 
-			Assert.Throws<Exception>(() =>
+			var err = Assert.Throws<NotImplementedException>(() =>
 			{
 				var archive = Archive.At("Example.zip");
 				Assert.IsTrue(archive.Contains("README.md"));
 			});
+
+			Assert.That (err.Message, Is.StringContaining("The requested feature is not implemented"), 
+			             "Expected that error becaause that's what you get when something has not been ported to Mono.");
 		}
 	}
 }
