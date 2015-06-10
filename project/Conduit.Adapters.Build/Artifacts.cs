78fdcb0f647809efadf787110b18f87288ad8226
Ben Biddington <ben.biddington@xero.com>
2015-06-10 16:55:31 +1200

    Using a .csproj and buld task to create archives

---
+++ project/Conduit.Adapters.Build/Artifacts.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.Build.Utilities;
+
+namespace Conduit.Adapters.Build
+{
+	public class Artifacts : Task
+	{
+		public string OutputDirectory { get; set; }
+
+		public override bool Execute()
+		{
+			var dir = OutputDirectory;
+
+			Cli.Say(BuildEngine, "Creating archive at <{0}>", dir);
+
+			UseCases.Archiving.Archive.At(dir);
+
+			return true;
+		}
+	}
+}
