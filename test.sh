39fa1d3399b477471b2ab25a543c591d9a24b07d
Ben Biddington <ben.biddington@gmail.com>
2016-02-20 16:36:16 +1300

    Add script for running all tests (which are failing currently)

---
+++ test.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+packages/xunit.runner.console.2.1.0/tools/xunit.console.exe project/Conduit.Unit.Tests/bin/Debug/Conduit.Unit.Tests.dll
+packages/xunit.runner.console.2.1.0/tools/xunit.console.exe project/Conduit.Integration.Tests/bin/Debug/Conduit.Integration.Tests.dll
