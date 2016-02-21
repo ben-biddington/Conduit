e83e9f7bb4d8c07de8351db7a766310a435e9065
Ben Biddington <ben.biddington@gmail.com>
2016-02-21 21:38:29 +1300

    Copy the tasks to new targets adapter

---
+++ project/Conduit.Build.Targets/List.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Execution;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace Conduit.Adapters.Build
+{
+	public class List : Task
+	{
+		public bool IncludeImported { get; set; }
+
+		public override bool Execute()
+		{
+			using (var reader = XmlReader.Create(new StringReader(File.ReadAllText(BuildEngine.ProjectFileOfTaskNode))))
+			{
+				var project = new Project(reader);
+
+				var targets = Filter(project);
+
+				Cli.Say(BuildEngine, "Project file <{0}> has the following <{1}> targets", BuildEngine.ProjectFileOfTaskNode, targets.Count);
+
+				foreach (ProjectTargetInstance target in targets)
+				{
+					Cli.Say(BuildEngine, "* {0}{1}", target.Name, Environment.NewLine);
+				}
+
+				return true;
+			}
+		}
+
+		private List<ProjectTargetInstance> Filter(Project project) 
+		{
+			if (IncludeImported)
+				return project.Targets.Values.ToList();
+
+			return project.Targets.Values.Where (it => it.Location.File.Equals(string.Empty)).OrderBy(it => it.Name).ToList();
+		}
+	}
+}
---
+++ project/Conduit.Build.Targets/Xunit.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.Collections;
+using Xunit.Runners;
+using System.Threading;
+
+namespace Conduit.Adapters.Build
+{
+	public class Xunit : Task
+	{
+		public String TestAssembly { get; set;}
+
+		/// <summary>
+		/// xbuild path/to/name.csproj /property:TestAssembly=path/to/AssemblyName.dll /t:T
+		/// </summary>
+		public override bool Execute()
+		{
+			// Is there an Xunit sdk for running tests programmatically?
+			// See: https://github.com/xunit/samples.xunit/blob/master/TestRunner/Program.cs
+
+			Cli.Say(BuildEngine, "Running test assembly <{0}>", TestAssembly);
+
+			Action<string> say = msg => Cli.Say(BuildEngine, msg);
+
+			string typeName = null; // https://github.com/xunit/xunit/blob/master/src/xunit.runner.utility/Runners/AssemblyRunner.cs#L173
+
+			var result = 0;
+
+			ManualResetEvent finished = new ManualResetEvent(false);
+
+			using (var runner = AssemblyRunner.WithAppDomain(TestAssembly))
+			{
+				runner.OnDiscoveryComplete 	= info => say("Running <" + info.TestCasesToRun + "> of <" + info.TestCasesDiscovered + "> tests found");
+				runner.OnExecutionComplete 	= info => { finished.Set(); say("Passed: " + (info.TotalTests - (info.TestsFailed + info.TestsSkipped))); };
+				runner.OnTestFailed 		= info => { result = 1;  say(info.ExceptionMessage); };
+				runner.OnTestOutput			= info => say(info.Output);
+				runner.OnTestPassed 		= info => say(info.Output);
+				runner.OnTestFinished 		= _ => say(".");
+				runner.OnTestSkipped 		= _ => say("*");
+
+				say("Discovering tests in assembly <" +  TestAssembly + ">");
+
+				runner.Start(typeName, parallel: false);
+
+				finished.WaitOne();
+				finished.Dispose();
+
+				say ("Tests finished with status <" + result + ">");
+
+				return result == 0;
+			}
+		}
+	}
+}
