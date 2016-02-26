bd327b6a3536a7ff7d2f8ed3cbd0dab20cc23904
Ben Biddington <ben.biddington@xero.com>
2016-02-26 13:08:54 +1300

    Add editor config

--- project/Conduit.Integration.Tests/Support/FileMachine.cs
+++ project/Conduit.Integration.Tests/Support/FileMachine.cs
@@ -3,47 +3,48 @@ using System.IO;
 
 namespace Conduit.Integration.Tests.Support
 {
-	internal static class FileMachine
-	{
-		public static FileInfo Touch(params string[] parts)
-		{
-		    var filename = new FileInfo(Path.Combine(parts));
+    internal static class FileMachine
+    {
+        public static FileInfo Touch(params string[] parts)
+        {
+            var filename = new FileInfo(Path.Combine(parts));
 
-		    Make(filename.FullName, string.Empty);
+            Make(filename.FullName, string.Empty);
 
-		    return filename;
-		}
+            return filename;
+        }
 
-	    public static void Touch(string filename)
-		{
-			Make(filename, string.Empty);
-		}
+        public static void Touch(string filename)
+        {
+            Make(filename, string.Empty);
+        }
 
-		public static FileInfo Make(string filename, string content)
-		{
-			var info = new FileInfo(Path.GetFullPath(filename).Replace("\\", Path.DirectorySeparatorChar.ToString()));
+        public static FileInfo Make(string filename, string content)
+        {
+            var info = new FileInfo(Path.GetFullPath(filename).Replace("\\", Path.DirectorySeparatorChar.ToString()));
 
-			EnsureDirectoryExists(info);
+            EnsureDirectoryExists(info);
 
-			Write(info, content);
+            Write(info, content);
 
-			return info;
-		}
+            return info;
+        }
 
-		static void Write(FileInfo filename, string content)
-		{
-				using (var s = File.OpenWrite(filename.FullName))
-				using (var writer = new StreamWriter (s)) {
-					writer.Write (content);
-					writer.Flush ();
-					writer.Close ();
-				}
-		}
+        static void Write(FileInfo filename, string content)
+        {
+            using (var s = File.OpenWrite(filename.FullName))
+            using (var writer = new StreamWriter(s))
+            {
+                writer.Write(content);
+                writer.Flush();
+                writer.Close();
+            }
+        }
 
-		private static void EnsureDirectoryExists(FileInfo info)
-		{
-			if (false == Directory.Exists(info.DirectoryName))
-				Directory.CreateDirectory(Path.GetFullPath(info.DirectoryName));
-		}
-	}
+        private static void EnsureDirectoryExists(FileInfo info)
+        {
+            if (false == Directory.Exists(info.DirectoryName))
+                Directory.CreateDirectory(Path.GetFullPath(info.DirectoryName));
+        }
+    }
 }
