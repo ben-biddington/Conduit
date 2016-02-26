34cb36a8136a3e8b361ab07deb6c13d8d64b2850
Ben Biddington <ben.biddington@gmail.com>
2016-02-26 20:32:00 +1300

    Pasing provided we turn appdomain off

--- project/Conduit.Adapters.Build/Xunit.cs
+++ project/Conduit.Adapters.Build/Xunit.cs
@@ -12,7 +12,7 @@ namespace Conduit.Adapters.Build
 
             var finished = new ManualResetEvent(false);
 
-            using (var runner = AssemblyRunner.WithAppDomain(testAssembly))
+            using (var runner = AssemblyRunner.WithoutAppDomain(testAssembly))
             {
                 Listen(report, testAssembly, runner, finished);
 
