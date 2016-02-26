80fbed4bba9a8a7722185df8f0640180c82ba13e
Ben Biddington <ben.biddington@gmail.com>
2016-02-26 20:29:07 +1300

    Prove that we can run xunit

---
+++ project/Conduit.Integration.Tests/TestRunning/Examples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Conduit.Adapters.Build;
+using Xunit;
+
+namespace Conduit.Integration.Tests
+{
+    public class Examples
+    {
+        [Fact]
+        public void this_one_passes()
+        {
+            Assert.False(Conduit.Adapters.Build.Xunit.Run(TestReport.Silent, Path.Combine("res", "Example.Unit.Tests.dll")));
+        }
+    }
+}
+
