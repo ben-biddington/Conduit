1dbafd6af65390109da9e29ad808513eafa9a388
Ben Biddington <ben.biddington@gmail.com>
2016-02-29 20:31:26 +1300

    Formatting

--- project/Conduit/UseCases/Semver/Assemblies/Private/Version/For.cs
+++ project/Conduit/UseCases/Semver/Assemblies/Private/Version/For.cs
@@ -9,44 +9,39 @@ using Conduit.UseCases.Semver.Assemblies.Private;
 
 namespace Conduit.UseCases.Semver.Assemblies.Private.Version
 {
-	internal static class For 
-	{
-		internal static SemVersion File(string filename, string prefix)
-		{
-			var pattern = new Regex(Matching.Pattern(prefix), RegexOptions.Compiled);
+    internal static class For
+    {
+        internal static SemVersion File(string filename, string prefix)
+        {
+            var pattern = new Regex (Matching.Pattern (prefix), RegexOptions.Compiled);
 
-			foreach (var line in Lines(filename))
-			{
-				var match = pattern.Match(line);
+            foreach (var line in Lines(filename)) {
+                var match = pattern.Match (line);
 
-				if (match.Success)
-				{
-					return SemVersionFrom(match);
-				}
-			}
+                if (match.Success) {
+                    return SemVersionFrom (match);
+                }
+            }
 
-			return new SemVersion(0);
-		}
+            return new SemVersion (0);
+        }
 
-		private static SemVersion SemVersionFrom(Match match)
-		{
-			var version = match.Groups["versionstring"].Value;
+        private static SemVersion SemVersionFrom(Match match)
+        {
+            var version = match.Groups ["versionstring"].Value;
 
-			try
-			{
-				return SemVersion.Parse(version);
-			}
-			catch (Exception)
-			{
-				throw new Exception("Failed to parse this text to version <" + version + ">");
-			}
-		}
+            try {
+                return SemVersion.Parse (version);
+            } catch (Exception) {
+                throw new Exception ("Failed to parse this text to version <" + version + ">");
+            }
+        }
 
-		private static IEnumerable<string> Lines(string filename) {
-			foreach (var line in System.IO.File.ReadAllLines(filename).Where(AssemblyInfoLine.IsInstruction))
-			{
-				yield return line;
-			}
-		}
-	}
+        private static IEnumerable<string> Lines(string filename)
+        {
+            foreach (var line in System.IO.File.ReadAllLines(filename).Where(AssemblyInfoLine.IsInstruction)) {
+                yield return line;
+            }
+        }
+    }
 }
