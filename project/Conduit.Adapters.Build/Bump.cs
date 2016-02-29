d9dbc6e220cdb5600054db1a104828864c436351
Ben Biddington <ben.biddington@gmail.com>
2016-02-29 20:11:09 +1300

    Add support for version incrementing

---
+++ project/Conduit.Adapters.Build/Bump.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Conduit.UseCases.Semver.Assemblies;
+
+namespace Conduit.Adapters.Build
+{
+    public static class Bump
+    {
+        public static void  Minor()
+        {
+            AssemblyVersion.BumpMajor("AssemblyInfo.cs");
+        }
+    }
+}
+
---
+++ project/Conduit.Build.Targets/Bump.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using Microsoft.Build.Utilities;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Evaluation;
+
+namespace Conduit.Build.Targets
+{
+    public class Bump : Task
+    {
+        public string Kind { get; private set; }
+
+        public Bump()
+        {
+            Kind = "Patch";
+        }
+
+        public override bool Execute()
+        {
+            if (Kind.Equals ("Minor", System.StringComparison.CurrentCultureIgnoreCase)) {
+                Conduit.Adapters.Build.Bump.Minor();
+            } else {
+                throw new Exception($"Unsupported bump kind <{Kind}>");
+            }
+            return true;
+        }
+    }
+}
+
