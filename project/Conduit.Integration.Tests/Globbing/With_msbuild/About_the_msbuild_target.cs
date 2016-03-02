a97f7e37dc41e1874edf02b7028c23598c58fd11
Ben Biddington <ben.biddington@gmail.com>
2016-03-02 15:03:46 +1300

    Refactor: move file

---
+++ project/Conduit.Integration.Tests/Globbing/With_msbuild/About_the_msbuild_target.cs
@@ -0,0 +1,24 @@
+﻿using Conduit.Integration.Tests.Support;
+using Xunit;
+using Glob = Conduit.Build.Targets.Glob;
+
+namespace Conduit.Integration.Tests.Globbing.With_msbuild
+{
+    public class About_the_msbuild_target : RunsInCleanRoom
+    {
+        [Fact]
+        public void pattern_may_be_an_array()
+        {
+            FileMachine.Touch("obj", "Debug", "example.dll");
+            FileMachine.Touch("obj", "Release", "example.dll");
+
+            var expected = FileMachine.Touch("example.dll");
+
+            var target = new Glob { Pattern = new [] { @".\xxx\example.dll;", @".\example.dll" } };
+
+            target.Execute();
+
+            Assert.Equal(expected.FullName, target.Path);
+        }
+    }
+}
