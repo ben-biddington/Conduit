c76ad246dea5b9c0ea1eb9ed120e59a4219b23ee
Ben Biddington <ben.biddington@gmail.com>
2016-03-02 14:40:07 +1300

    Glob pattern may be an array

--- project/Conduit.Build.Targets/Glob.cs
+++ project/Conduit.Build.Targets/Glob.cs
@@ -7,14 +7,15 @@ namespace Conduit.Build.Targets
     public class Glob : Task
     {
         [Required]
-        public string Pattern { get; set; }
+        public string[] Pattern { get; set; }
        
         [Output]
         public string Path { get; private set;}
 
         public override bool Execute()
         {
-            Path = Dir.Newest(new Adapters.Build.Glob(Pattern)).FullName;
+            Path = Dir.Newest(new Adapters.Build.Glob(Pattern[0])).FullName;
+
             return true;
         }
     }
