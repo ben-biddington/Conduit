869280c718504b8b144f05023327d35879ed47a8
Ben Biddington <ben.biddington@gmail.com>
2016-03-04 11:57:06 +1300

    Formatting

--- project/Conduit.Build.Targets/List.cs
+++ project/Conduit.Build.Targets/List.cs
@@ -10,34 +10,34 @@ using System.IO;
 namespace Conduit.Build.Targets
 {
     public class List : Task
-	{
-		public bool IncludeImported { get; set; }
+    {
+        public bool IncludeImported { get; set; }
 
-		public override bool Execute()
-		{
-			using (var reader = XmlReader.Create(new StringReader(File.ReadAllText(BuildEngine.ProjectFileOfTaskNode))))
-			{
-				var project = new Project(reader);
+        public override bool Execute()
+        {
+            using (var reader = XmlReader.Create(new StringReader(File.ReadAllText(BuildEngine.ProjectFileOfTaskNode))))
+            {
+                var project = new Project(reader);
 
-				var targets = Filter(project);
+                var targets = Filter(project);
 
-				Cli.Say(BuildEngine, "Project file <{0}> has the following <{1}> targets", BuildEngine.ProjectFileOfTaskNode, targets.Count);
+                Cli.Say(BuildEngine, "Project file <{0}> has the following <{1}> targets", BuildEngine.ProjectFileOfTaskNode, targets.Count);
 
-				foreach (ProjectTargetInstance target in targets)
-				{
-					Cli.Say(BuildEngine, "* {0}{1}", target.Name, Environment.NewLine);
-				}
+                foreach (ProjectTargetInstance target in targets)
+                {
+                    Cli.Say(BuildEngine, "* {0}{1}", target.Name, Environment.NewLine);
+                }
 
-				return true;
-			}
-		}
+                return true;
+            }
+        }
 
-		private List<ProjectTargetInstance> Filter(Project project) 
-		{
-			if (IncludeImported)
-				return project.Targets.Values.ToList();
+        private List<ProjectTargetInstance> Filter(Project project)
+        {
+            if (IncludeImported)
+                return project.Targets.Values.ToList();
 
-			return project.Targets.Values.Where (it => it.Location.File.Equals(string.Empty)).OrderBy(it => it.Name).ToList();
-		}
-	}
+            return project.Targets.Values.Where(it => it.Location.File.Equals(string.Empty)).OrderBy(it => it.Name).ToList();
+        }
+    }
 }
