d0752540114c6277225c97e65389067fce3603d0
Ben Biddington <ben.biddington@gmail.com>
2016-03-03 15:06:11 +1300

    Delete references

--- project/Conduit.Adapters.Build/Packaging/Nuget.cs
+++ project/Conduit.Adapters.Build/Packaging/Nuget.cs
@@ -15,16 +15,17 @@ namespace Conduit.Adapters.Build.Packaging
 
         public static IEnumerable<NugetPackage> Find(Uri uri, string id)
         {
-            var repo = PackageRepositoryFactory.Default.CreateRepository(uri.AbsoluteUri);
-
-            return repo.FindPackagesById(id).Select(it => new NugetPackage(it.Id));
+            return PackageRepository(uri).FindPackagesById(id).Select(it => new NugetPackage(it.Id));
         }
 
         public static void Install(Uri uri, string id, string version, DirectoryInfo directory)
         {
-            var repo = PackageRepositoryFactory.Default.CreateRepository(uri.AbsoluteUri);
+            new PackageManager(PackageRepository(uri), directory.FullName).InstallPackage(id, SemanticVersion.Parse(version));
+        }
 
-            new PackageManager(repo, directory.FullName).InstallPackage(id, SemanticVersion.Parse(version));
+        private static IPackageRepository PackageRepository(Uri uri)
+        {
+            return PackageRepositoryFactory.Default.CreateRepository(uri.AbsoluteUri);
         }
     }
 
