116cf65f9d40bff848cec53b58157e9b4ebc340b
Ben Biddington <ben.biddington@gmail.com>
2016-03-04 10:45:07 +1300

    Split file

--- project/Conduit.Integration.Tests/Packaging/Flattening/Can_flatten_a_package_to_a_single_directory.cs
+++ project/Conduit.Integration.Tests/Packaging/Flattening/Can_flatten_a_package_to_a_single_directory.cs
@@ -1,42 +1,10 @@
-using System.Collections.Generic;
 using System.IO;
 using Conduit.Adapters.Build.Packaging;
 using Conduit.Integration.Tests.Support;
 using Xunit;
-using System.Linq;
 
 namespace Conduit.Integration.Tests.Packaging.Flattening
 {
-    public class Can_read_a_packages_config_file : RunsInCleanRoom
-    {
-        [Fact]
-        public void for_example() {
-            var xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
-                        <packages>
-                            <package id=""Microsoft.Web.Xdt"" version=""2.1.0"" targetFramework=""net45"" />
-                            <package id=""Newtonsoft.Json"" version=""6.0.4"" targetFramework=""net45"" />
-                            <package id=""xunit"" version=""2.1.0"" targetFramework=""net45"" />
-                            <package id=""xunit.core"" version=""2.1.0"" targetFramework=""net45"" />
-                        </packages>";
-
-            var file = FileMachine.Make("packages.config", xml);
-
-            var packages = PackagesConfig.Read(file);
-
-            Assert.True(packages.Any(it => it.Id.Equals("xunit")), string.Join(",", packages.Select(it => it.Id)));
-        }
-
-        [Fact]
-        public void it_returns_empty_when_file_does_not_exist() {
-            var packages = PackagesConfig.Read(new FileInfo("xxx-does-not-exist-xxx"));
-
-            Assert.Equal(0, packages.Count);
-        }
-
-        // TEST: Should really be able to supply text rather than file
-        // TEST: Check that it returns all the right values
-    }
-
     public class Can_flatten_a_package_to_a_single_directory : RunsInCleanRoom
     {
         [Fact]
