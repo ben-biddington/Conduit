73a120782fb6e74d792e455219865ab6dde04f79
Ben Biddington <ben.biddington@gmail.com>
2016-03-10 12:51:38 +1300

    Split file

--- project/Conduit.Build.Targets/Nuget/Flatten.cs
+++ project/Conduit.Build.Targets/Nuget/Flatten.cs
@@ -9,44 +9,6 @@ using Microsoft.Build.Framework;
 
 namespace Conduit.Build.Targets.Nuget
 {
-    public class Install : Task
-    {
-        /// <summary>
-        /// Where the packages.config file is
-        /// </summary>
-        [Required]
-        public string PackagesConfigFile { get; set; }
-
-        /// <summary>
-        /// Where nuget server is, defaults to `https://packages.nuget.org/api/v2`
-        /// </summary>
-        public string NugetUrl { get; set; }
-
-        /// <summary>
-        /// When true, installs packages along with their depdencies
-        /// </summary>
-        public bool IncludeDependencies { get; set; }
-
-        /// <summary>
-        /// Where you want to install packages to
-        /// </summary>
-        [Required]
-        public string TargetDirectory { get; set; }
-
-        public override bool Execute()
-        {
-            var packages = PackagesConfig.Read(new FileInfo(PackagesConfigFile));
-
-            Adapters.Build.Packaging.Nuget.Install(
-              new Uri(NugetUrl),
-              new DirectoryInfo(TargetDirectory),
-              new Adapters.Build.Packaging.Nuget.InstallOptions(IncludeDependencies),
-              packages.ToArray());
-
-            return true;
-        }
-    }
-
     public class Flatten : Task
     {
         /// <summary>
